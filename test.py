c8ecb1dd72c2c7ec4e89019341007f020a81cfb6
yasser nadir <cbdb4716@orange.com>
2024-11-24 19:12:06 +0100

    On going

--- test.py
+++ test.py
@@ -1,10 +1,44 @@
-from weasyprint import HTML
+import requests
+from requests.auth import HTTPBasicAuth
 
-html_content = """
-<html>
-<head><title>Test PDF</title></head>
-<body><h1>Hello, World!</h1></body>
-</html>
-"""
+url = 'https://10.253.55.155'
+path = '/policy/api/v1/infra/lb-virtual-servers'
+username = 'admin'
+password = 'Ecocenter777*'
 
-HTML(string=html_content).write_pdf('output.pdf')
+
+response = requests.get(url+path, auth=HTTPBasicAuth(username, password), verify=False)
+
+if response.status_code == 200:
+    json_data = response.json()
+    print("Response JSON:")
+    for result in json_data['results']:
+        if result['display_name'].find('VIP') != -1:
+            print(f'name: {result['display_name']}, ports: {[port for port in result['ports'] if int(port)>=50000]}')
+    
+else:
+    print(f"Failed to retrieve data: {response.status_code}")
+    print("Response:", response.text)
+
+
+# import aiohttp
+# import asyncio
+
+# url = 'https://10.253.55.155'
+# path = '/policy/api/v1/infra/lb-virtual-servers'
+# username = 'admin'
+# password = 'Ecocenter777*'
+
+# async def fetch_data(url, username, password):
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url, auth=aiohttp.BasicAuth(username, password), ssl=False) as response:
+#             if response.status == 200:
+#                 json_data = await response.json()
+#                 print("Response JSON:")
+#                 for result in json_data['results']:
+#                     print(f'name: {result['display_name']}, ports: {result['ports']}')
+#             else:
+#                 print(f"Failed to retrieve data: {response.status}")
+#                 print("Response:", await response.text())
+
+# asyncio.run(fetch_data(url+path, username, password))
